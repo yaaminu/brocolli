0fd91c2252ae7332bb9d6e96bbf55fa87dec09ba
Aminu Abdul Manaf <aminu@mpharma.com>
2021-03-23 15:19:52 +0000

    Render real world react app

--- main.py
+++ main.py
@@ -2,25 +2,20 @@ import os
 from pathlib import Path
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 import brocolli
 
 app = FastAPI()
 react_renderer = brocolli.create_renderer(type='react')
 
+app.mount("/static", StaticFiles(directory="./app/build/static"), name="static")
+
 
 @app.get("/")
 async def root():
     app_path = Path(os.curdir, "build/sample_app.js").resolve()
-    css = Path('./index.css').read_text("utf-8")
+    html = Path('./app/build/index.html').read_text("utf-8")
     rendered_app = react_renderer.render_app(app_path)
-    content = f"""
-    <html>
-        <head>
-            <style>
-                {css}
-            </style> 
-        </head>
-        <body> {rendered_app}  </body>
-    </html>"""
+    content = html.replace('<div id="root"></div>', f"<div id='root'>{rendered_app}</div>")
     return HTMLResponse(content=content)
