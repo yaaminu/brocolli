cdf1bd95222772259e255c8fe8b007a1b39f3b7b
Aminu Abdul Manaf <afolanyaaminu@gmail.com>
2021-04-17 17:31:23 +0000

    resolve merge conflict mess

--- brocolli/react_renderer.js
+++ brocolli/react_renderer.js
@@ -9,7 +9,6 @@ module.exports = function render(app_module, data){
         App = module
     }
     props = data || {}
-    console.log(JSON.stringify(props))
     return {
         "markup": react_dom.renderToString(react.createElement(App, props)),
         "state": JSON.stringify(global.___brocolli___.state),
